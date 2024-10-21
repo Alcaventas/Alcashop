a95d99cbf28eb4b16dc6d8a876cb5e8c94d972cf
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-10-21 02:50:45 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -186,7 +186,7 @@ if (opts['server']) (await import('./server.js')).default(global.conn, PORT)
 async function clearTmp() {
 const tmp = [tmpdir(), join(__dirname, './tmp')]
 const filename = []
-tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+tmp.forEach((dirname) => readdirSync(dirname).forEach((file) => filename.push(join(dirname, file))));
 
 return filename.map(file => {
 const stats = statSync(file)
